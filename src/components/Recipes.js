31e935b5754e7b4bfdf6411ac0b0b972488e8e42
HopeEternal <risalee316@gmail.com>
2019-10-10 12:54:53 -0600

    API Request Initialized

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import axios from 'axios'
 
-export default function Recipes() {
-    return (
-        <div>
-            <h2>Recipes Here</h2>
-        </div>
-    )
+export default class Recipes extends React.Component {
+    state = {
+        persons: []
+    }
+
+    componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+        .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+        })
+    }
+    render() {
+        return (
+            <div>
+                <h2>Recipes Here</h2>
+                <ul>
+                { this.state.persons.map(person => <li>{person.name}</li>)}
+                </ul>
+            </div>
+        )
+    }
 }
+
