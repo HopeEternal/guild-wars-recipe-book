42bd971bcb703cba9c49b8c80ce58af6c756618e
HopeEternal <risalee316@gmail.com>
2019-10-10 18:02:00 -0600

    GW2 API Fetch complete, draft code for recipe name output

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -8,42 +8,62 @@ export default class Recipes extends React.Component {
     }
 
     componentDidMount() {
+    //Grab and Store Recipe IDs
     axios.get(`https://api.guildwars2.com/v2/recipes`)
-        .then(res => { this.setState({ recipeIDs: res.data });
-          //  console.log(this.state.recipeIDs);
-
+        .then(res => { this.setState({ recipeIDs: res.data })
+            //Grab Recipes by ID
             this.state.recipeIDs.forEach((element) => {
+                if (element > 9200 && element < 9210) {
                     axios.get(`https://api.guildwars2.com/v2/recipes/` + element)
                     .then(res => {
-                        res.data.disciplines.includes("Weaponsmith") ? this.state.recipes.push(res.data) : console.log("POOP!");
-                    
-                 //   this.setState({ recipes });
-                    console.log(this.state.recipes);
+                       if (res.data.disciplines.includes("Chef")) {
+                           this.setState({ recipes: [...this.state.recipes, res.data]})
+                           // console.log(this.state.recipes);
+                       } 
                     })  
-                    .catch(
-                        console.log(err);
-                    )
-                             
+                    .catch((error) => console.log(error))
+                }             
             })
 
+            setTimeout(() => {
+                alert("Hello"); 
+               this.state.recipes.forEach((element) => {
+                axios.get(`https://api.guildwars2.com/v2/items?ids=` + element.output_item_id)
+                .then(res => {
+                    console.log(res.data);
+                    
+                })
+            })
+                
+              }, 2000);
+
+            
+            
         })
+        .catch((error) => console.log(error))
+        //Grab item names
+        
     }
     render() {
         return (
             <div style={recipeCont} className="container">
-                {
-                    /* <div className="row">
+                {/* <i className="material-icons cake right">cake</i>
+                <i className="material-icons free_breakfast right">free_breakfast</i>
+                <i className="material-icons local_bar right">local_bar</i>
+                <i className="material-icons local_dining right">local_dining</i> */}
+                <div className="row">
                     { this.state.recipes.map(recipe => 
-                    <div className="col s12 m6 l3">
-                        <div className="card-panel hoverable">
-                            <div className="card-content">
-                                <i className="material-icons palette right">palette</i>
-                                <p className="blue-grey-text-darken-3">{recipe.type}</p>
+                        <div className="col s12 m6 l3">
+                            <div className="card-panel hoverable">
+                                <div className="card-content">
+                                    <i className="material-icons local_dining right">local_dining</i>
+                                    <p className="blue-grey-text-darken-3"><strong>Type:</strong> {recipe.type}</p>
+                                    <p><strong>Time to craft</strong>: {recipe.time_to_craft_ms}ms</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
                     )}
-                </div> */}
+                </div>
             </div>
         )
     }
